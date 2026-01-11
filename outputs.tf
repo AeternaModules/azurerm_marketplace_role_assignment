5226f185c9f9e0d7d13084cebe7c1777c72dbb5f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:42:29 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "marketplace_role_assignments" {
+  description = "All marketplace_role_assignment resources"
+  value       = azurerm_marketplace_role_assignment.marketplace_role_assignments
+}
+
